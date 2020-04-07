9b36096bbcd2cfd9957ac8e2ac848577a60e05b5
aiinsight <min.hk@aiinsight.io>
2020-04-07 11:14:05 +0900

    main view complete

--- app/src/main/java/com/jjonami/videosample/MainActivity.kt
+++ app/src/main/java/com/jjonami/videosample/MainActivity.kt
@@ -2,11 +2,28 @@ package com.jjonami.videosample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setDisableCapture(true)
+        setScreenOn(true)
+
+        savedInstanceState ?: supportFragmentManager.beginTransaction()
+                .replace(R.id.container, RecordingFragment())
+                .commit()
+    }
+
+    private fun setDisableCapture(isCapture: Boolean){
+        if(isCapture) window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+    }
+
+    private fun setScreenOn(isScreenOn: Boolean){
+        if(isScreenOn) window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
